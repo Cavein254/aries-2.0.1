00f5a46f68cbd8458a3cdf6ef394eeceed04c2f7
johnmuthua <caveomuthama@gmail.com>
2023-08-21 12:49:32 +0300

    Add reduxtoolkit

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,21 +1,24 @@
-import { Box, TextField } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import './styles.css';
 
 const Login = () => {
   return (
-    <Box>
-      <Box></Box>
-      <Box>
-        <TextField
-          variant="standard"
-          placeholder="Enter your Email Address"
-          type="email"
-        />
-        <TextField
-          variant="standard"
-          placeholder="Enter your Password"
-          type="password"
-        />
+    <Box className="auth">
+      <Box className="auth-container">
+        <Box></Box>
+        <form className="auth-form">
+          <TextField
+            variant="standard"
+            placeholder="Enter your Email Address"
+            type="email"
+          />
+          <TextField
+            variant="standard"
+            placeholder="Enter your Password"
+            type="password"
+          />
+          <Button variant="contained">Login</Button>
+        </form>
       </Box>
     </Box>
   );
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,5 +1,37 @@
-const Register = () => {
-  return <div>Register</div>;
+import { Box, Button, TextField } from '@mui/material';
+import './styles.css';
+
+const Login = () => {
+  return (
+    <Box className="auth">
+      <Box className="auth-container">
+        <Box></Box>
+        <form className="auth-form">
+          <TextField
+            variant="standard"
+            placeholder="Enter your user name"
+            type="text"
+          />
+          <TextField
+            variant="standard"
+            placeholder="Enter your Email Address"
+            type="email"
+          />
+          <TextField
+            variant="standard"
+            placeholder="Enter your Password"
+            type="password"
+          />
+          <TextField
+            variant="standard"
+            placeholder="Re-enter your password"
+            type="password"
+          />
+          <Button variant="contained">Login</Button>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
-export default Register;
+export default Login;
